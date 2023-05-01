0aedfaeff21c6ad217f712a989eda7bdc9537916
Tensys57 <tensys57@gmail.com>
2023-05-02 00:19:00 +0300

    Task 8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,19 @@
+// Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
+// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const player = new Player(iframe);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const returnPoint = localStorage.getItem('videoplayer-current-time');
 
-iframe.addEventListener(`timeupdate`, onCurrentTimeHandler);
-console.log('objetimeupdatect :>> ', obtimeupdateject);
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const checkReturnPoint = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.setCurrentTime(returnPoint);
+player.on(`timeupdate`, throttle(checkReturnPoint, 1000));
