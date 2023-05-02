a864dac78cb0e135638ef97ea72c8e13d696964c
Tensys57 <tensys57@gmail.com>
2023-05-02 16:55:39 +0300

    Task 8-3 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+// HTML містить розмітку форми. Напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
+import throttle from 'lodash.throttle';
+const LOCAL_KEY = 'feedback-form-state';
+const ref = {
+  formData: document.querySelector('.feedback-form'),
+  inputValue: document.querySelector('input'),
+  textareaValue: document.querySelector('textarea'),
+  submitBtn: document.querySelector('button'),
+};
+
+function inputHandler(event) {
+  const { email, message } = event.currentTarget.elements;
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+const formDataRecovery = JSON.parse(localStorage.getItem(LOCAL_KEY));
+
+if (formDataRecovery) {
+  ref.inputValue.value = formDataRecovery.email;
+  ref.textareaValue.textContent = formDataRecovery.message;
+}
+
+function submitHandler(ev) {
+  ev.preventDefault();
+  console.log('email:>> ', ref.formData.email.value);
+  console.log('message:>>', ref.formData.message.value);
+  ref.inputValue.value = '';
+  ref.textareaValue.value = '';
+  localStorage.removeItem(LOCAL_KEY);
+}
+ref.formData.addEventListener('input', inputHandler);
+ref.submitBtn.addEventListener('click', throttle(submitHandler, 500));
